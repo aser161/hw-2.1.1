afa3132d26145290b30f5be92117ea2131a2d059
aser161 <aser161@yandex.ru>
2022-12-17 14:34:27 +0300

    Check HW

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,11 @@
 public class Car {
 
-    String brand;
-    String model;
-    float engineVolume;
-    String color;
-    int year;
-    String country;
+    private String brand;
+    private String model;
+    private float engineVolume;
+    private String color;
+    private int year;
+    private String country;
 
     Car(String brand, String model, float engineVolume, String color, int year, String country){
         this.brand = brand;
